4331dc85422eb4cbd1b7ad50d5304a82757d551e
anastasiatoullec <106024463+anastasiatoullec@users.noreply.github.com>
2022-07-08 23:02:39 +0200

    Update sql

--- dataset.py
+++ dataset.py
@@ -1,18 +1,19 @@
+#pip install sqlalchemy
+
 import sqlite3, sqlalchemy
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData, create_engine, text, inspect, Float
 import joblib
-
 
 engine = create_engine("sqlite:///credit.db")
 meta = MetaData()
 credit= Table(
     'credit', meta, 
-    Column('ID', String, primary_key=True), #chiffres+tirets==String?
+    Column('ID', String, primary_key=True), 
     Column('Customer_ID', String), 
     Column('Month', String),
     Column('Name', String),
-    Column('Age', Float), #Integer?
-    Column('SSN', String),
+    Column('Age', Integer), 
+    Column('SSN', String), 
     Column('Occupation', String), 
     Column('Annual_Income', Float),
     Column('Monthly_Inhand_Salary', Float), 
@@ -22,24 +23,28 @@ credit= Table(
     Column('Num_of_Loan', Integer), 
     Column('Type_of_Loan', String),
     Column('Delay_from_due_date', Integer), 
-    Column('Num_of_Delayed_Payment', Integer),
-    Column('Changed_Credit_Limit', Float), 
+    Column('Num_of_Delayed_Payment', Integer), 
+    Column('Changed_Credit_Limit', Float),
     Column('Num_Credit_Inquiries', Float),
     Column('Credit_Mix', String),
     Column('Outstanding_Debt', Float),
     Column('Credit_Utilization_Ratio', Float), 
-    Column('Credit_History_Age', String),
     Column('Payment_of_Min_Amount', String),
     Column('Total_EMI_per_month', Float),
     Column('Amount_invested_monthly', Float), 
     Column('Payment_Behaviour', String),
-    Column('Monthly_Balance', Float)
-    
+    Column('Monthly_Balance', Float),
+    Column('Credit_History_Age_Years', Integer),
+    Column('Credit_History_Age_Months', Integer)    
 )
 meta.create_all(engine)
 
-values = joblib.load('./data.pkl')#file with data cleaning
 
+
+values = joblib.load('./df.pkl')#file with clean data
+
+
+#Need transformed values!!!
 with engine.connect() as connection:
     with connection.begin() as transaction:
         try:
@@ -56,6 +61,5 @@ with engine.connect() as connection:
             raise
         else:
             transaction.commit()
-
 
 conn = engine.connect() #Instantiate the Connection class in a variable named conn
