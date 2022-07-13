e7aaa84a0f1b1a32195fc4cbd0e5f9016e405229
anastasiatoullec <106024463+anastasiatoullec@users.noreply.github.com>
2022-07-13 21:37:46 +0200

    Creation of two tables

--- sql_db.py
+++ sql_db.py
@@ -4,21 +4,27 @@ import sqlite3, sqlalchemy
 from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData, create_engine, text, inspect, Float
 import joblib
 
-engine = create_engine("sqlite:///credit.db")
+engine = create_engine("sqlite:///credit_customer.db")
+
 meta = MetaData()
-credit= Table(
-    'credit', meta, 
+
+customer= Table(
+    'customer', meta, 
     Column('ID', String, primary_key=True), 
     Column('Customer_ID', String), 
-    Column('Month', String),
     Column('Name', String),
     Column('Age', Integer), 
     Column('SSN', String), 
     Column('Occupation', String), 
     Column('Annual_Income', Float),
-    Column('Monthly_Inhand_Salary', Float), 
+    Column('Monthly_Inhand_Salary', Float)        
+)
+
+credit= Table(
+    'credit', meta, 
+    Column('ID_Credit', Integer, primary_key=True),
+    Column('ID', String, ForeignKey("customer.ID")), 
     Column('Num_Bank_Accounts', Integer),
-    Column('Num_Credit_Card', Integer), 
     Column('Interest_Rate', Integer),
     Column('Num_of_Loan', Integer), 
     Column('Type_of_Loan', String),
@@ -37,10 +43,12 @@ credit= Table(
     Column('Credit_History_Age_Years', Integer),
     Column('Credit_History_Age_Months', Integer)    
 )
+
 meta.create_all(engine)
 
-df = joblib.load('./df.pkl')#file with clean data
-values = list(zip(*map(df.get, df)))
+df_customer = joblib.load('./df_customer.pkl')#file with clean data
+values = list(zip(*map(df_customer.get, df_customer)))
+
 
 with engine.connect() as connection:
     with connection.begin() as transaction:
@@ -50,7 +58,7 @@ with engine.connect() as connection:
             # We use the SQL language in text format where markers is the format of a tuple
             ins = 'INSERT INTO {tablename} VALUES ({markers})'
             # This particular format is specified using the format member function
-            ins = ins.format(tablename=credit.name, markers=markers)
+            ins = ins.format(tablename=customer.name, markers=markers)
             # Finally we can use the tuples created by executing the SQL command
             connection.execute(ins, values)
         except:
@@ -59,7 +67,24 @@ with engine.connect() as connection:
         else:
             transaction.commit()
 
-conn = engine.connect() #Instantiate the Connection class in a variable named conn
+df_credit = joblib.load('./df_credit.pkl')#file with clean data
+values_credit = list(zip(*map(df_credit.get, df_credit)))
 
-joblib.dump(conn, './conn.pkl')
+with engine.connect() as connection:
+    with connection.begin() as transaction:
+        try:
+            # We indicate the format of a tuple of this table
+            markers = ','.join('?' * len(values_credit[0])) 
+            # We use the SQL language in text format where markers is the format of a tuple
+            ins = 'INSERT INTO {tablename} VALUES ({markers})'
+            # This particular format is specified using the format member function
+            ins = ins.format(tablename=credit.name, markers=markers)
+            # Finally we can use the tuples created by executing the SQL command
+            connection.execute(ins, values_credit)
+        except:
+            transaction.rollback()
+            raise
+        else:
+            transaction.commit()
 
+conn = engine.connect()
